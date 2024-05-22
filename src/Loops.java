e5b7db5191ef23d3221daf4ad7a2110a2978db35
Laura <laurisalmawa05gmail.com>
2024-05-22 14:06:01 +0200

    Adding base to the tree made out of MaxSymbols

--- src/Loops.java
+++ src/Loops.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Loops {
 
     public static void main(String[] args) {
-        Loops lauraTheSeller = new Loops();
+        Loops loops = new Loops();
         boolean exit = true;
 
         do {
@@ -19,45 +19,35 @@ public class Loops {
             int option = scanner.nextInt();
 
 
-            switch(option) {
+            switch (option) {
                 case 1:
-                    lauraTheSeller.sellNewspapers();
+                    loops.sellNewspapers();
                     break;
                 case 2:
-                    lauraTheSeller.countNextNumbers();
+                    loops.countNextNumbers();
                     break;
                 case 3:
-                    lauraTheSeller.countPreviousNumbers();
+                    loops.countPreviousNumbers();
                     break;
                 case 4:
-                    lauraTheSeller.printHalfTree();
+                    loops.printHalfTree();
                     break;
                 case 5:
-                    System.out.println("Choose the option you want to use:");
-                    System.out.println("1. Paint Symbols by the max number you enter.");
-                    System.out.println("2. Paint Symbols by the max lines you enter.");
-                    Scanner scanner1 = new Scanner(System.in);
-                    int optionTree = scanner1.nextInt();
-
-                    switch (optionTree) {
-                        case 1:
-                            lauraTheSeller.paintSymbolsByMaxNumber();
-                        break;
-                        case 2:
-                            lauraTheSeller.paintSymbolsByMaxLines();
-                        break;
-                        default:
-                    }
+                    exit = loops.paintATree();
+                    break;
                 default:
+
                     exit = false;
             }
-        } while(exit);
+
+        }while (exit) ;
 
     }
+
     public void sellNewspapers() {
         System.out.println("LAURA SELLING NEWSPAPERS");
         int energy = 100;
-        int newspaper = (int)Math.floor(Math.random()*(50 - 30) + 30);
+        int newspaper = (int) Math.floor(Math.random() * (50 - 30) + 30);
         System.out.println("Choose the option to execute: ");
         System.out.println("1. while");
         System.out.println("2. for");
@@ -67,29 +57,29 @@ public class Loops {
         Scanner scanner = new Scanner(System.in);
         int option = scanner.nextInt();
 
-        switch(option) {
+        switch (option) {
             case 1:
                 while (newspaper > 0 && energy > 0) {
                     int newsToSell = (int) Math.floor(Math.random() * (5 - 1) + 1);
                     System.out.println("Laura has " + newspaper + " newspapers to sell. And she has " + energy
-                        + "% of energy, she is going to sell " + newsToSell + " newspapers.");
+                            + "% of energy, she is going to sell " + newsToSell + " newspapers.");
 
                     if (newsToSell <= newspaper) {
-                    newspaper -= newsToSell;
+                        newspaper -= newsToSell;
                     } else {
-                       newspaper -= newspaper;
-                     }
-                energy = energy - 5;
+                        newspaper -= newspaper;
                     }
+                    energy = energy - 5;
+                }
                 System.out.println("Laura has " + energy + "% of energy, and she has left " + newspaper + " newspaper to sell");
-                if(newspaper == 0) {
+                if (newspaper == 0) {
                     System.out.println("Congratulations Laura! you have sold all the newspapers.");
                 } else {
                     System.out.println("I'm really sorry for you Laura, better luck for the next one!");
                 }
                 break;
             case 2:
-                for(; newspaper > 0 && energy > 0; energy -= 5) {
+                for (; newspaper > 0 && energy > 0; energy -= 5) {
                     int newsToSell = (int) Math.floor(Math.random() * (5 - 1) + 1);
                     System.out.println("Laura has " + newspaper + " newspapers to sell. And she has " + energy
                             + "% of energy, she is going to sell " + newsToSell + " newspapers.");
@@ -100,12 +90,12 @@ public class Loops {
                         newspaper -= newspaper;
                     }
                 }
-                    System.out.println("Laura has " + energy + "% of energy, and she has left " + newspaper + " newspaper to sell");
-                    if (newspaper == 0) {
-                        System.out.println("Congratulations Laura! you have sold all the newspapers.");
-                    } else {
-                        System.out.println("I'm really sorry for you Laura, better luck for the next one!");
-                    }
+                System.out.println("Laura has " + energy + "% of energy, and she has left " + newspaper + " newspaper to sell");
+                if (newspaper == 0) {
+                    System.out.println("Congratulations Laura! you have sold all the newspapers.");
+                } else {
+                    System.out.println("I'm really sorry for you Laura, better luck for the next one!");
+                }
 
                 break;
             case 3:
@@ -119,7 +109,7 @@ public class Loops {
     public void countNextNumbers() {
         System.out.println("COUNTING FROM LOWEST TO HIGHEST");
         System.out.println(" ");
-        System.out.print("Write a positive number to count from lowest to highest: " );
+        System.out.print("Write a positive number to count from lowest to highest: ");
         Scanner scanner = new Scanner(System.in);
         int number = Integer.parseInt(scanner.next());
         System.out.println("Choose the option to execute: ");
@@ -130,15 +120,15 @@ public class Loops {
         Scanner scanner1 = new Scanner(System.in);
         int option = scanner1.nextInt();
 
-        switch(option) {
+        switch (option) {
             case 1:
                 for (int counter = 1; counter <= number; counter++) {
-                System.out.println(counter);
+                    System.out.println(counter);
                 }
                 break;
             case 2:
                 int counter = 1;
-                while(counter <= number){
+                while (counter <= number) {
                     System.out.println(counter++);
                 }
             case 3:
@@ -166,7 +156,7 @@ public class Loops {
                     System.out.println(number--);
                 }
             case 2:
-                for(;number > 0; number -= number) {
+                for (; number > 0; number -= number) {
                     System.out.println(number);
                 }
                 break;
@@ -181,8 +171,8 @@ public class Loops {
         Scanner scanner = new Scanner(System.in);
         int lines = scanner.nextInt();
 
-        for(int counter = 1; counter <= lines; counter++){
-            for(int counter2 = 1; counter >= counter2; counter2++) {
+        for (int counter = 1; counter <= lines; counter++) {
+            for (int counter2 = 1; counter >= counter2; counter2++) {
                 System.out.print("*");
             }
             System.out.println(" ");
@@ -190,56 +180,87 @@ public class Loops {
     }
 
 
-    public void paintSymbolsByMaxNumber() {
-        System.out.println("Enter the number of asterisks that you want on your tree:");
+    public boolean paintATree() {
+
+        System.out.println("Choose the option you want to use:");
+        System.out.println("1. Paint Symbols by the max number you enter.");
+        System.out.println("2. Paint Symbols by the max lines you enter.");
+        System.out.println("3. Get back to the previous menu.");
+        System.out.println("Any other number to exit.");
+        // Quedarse en el menu y opcion 3 para volver al menu anterior y cualquier otra tecla para salir
+        //Meterle a todos los arboles base, base despues de dos niveles,
+        // base impar
+        // 3/2 = 1
+        // 7/2 = 3
         Scanner scanner = new Scanner(System.in);
-        int maxSymbols = scanner.nextInt();
-        int lines = maxSymbols/2 +1;
-        int spaces = maxSymbols/2;
+        int optionTree = scanner.nextInt();
+        boolean exit = true;
 
-        for(int countLines = 1, symbols = 1; countLines <= lines; countLines++, symbols+=2, spaces--) {
-            for(int countSpaces = 1; spaces >= countSpaces ; countSpaces++) {
-                System.out.print(" ");
+            switch (optionTree) {
+                case 1:
+                    System.out.println("Enter the number of asterisks that you want on your tree:");
+                    Scanner scanner1 = new Scanner(System.in);
+                    int maxSymbols = scanner1.nextInt();
+                    int lines = (maxSymbols % 2 == 1)? maxSymbols/2+1 : maxSymbols/2;
+                    int spaces = maxSymbols / 2;
+                    int baseLines = lines / 2;
+                    int baseSpaces = maxSymbols / 3;
+                    int baseSymbols = maxSymbols - 2*baseSpaces;
+
+                    for (int countLines = 1 , symbols = 1; countLines <= lines; countLines++, symbols += 2, spaces--) {
+                        for (int countSpaces = 1; spaces >= countSpaces; countSpaces++) {
+                            System.out.print(" ");
+                        }
+                        for (int countSymbols = (maxSymbols % 2 == 1) ? 1 : 2; countSymbols <= symbols; countSymbols++) {
+                            System.out.print("*");
+                        }
+                        System.out.println(" ");
+                    }
+                    for (int countBaseLines = 1; countBaseLines <= baseLines; countBaseLines++) {
+                        for (int countBaseSpaces = 1; countBaseSpaces <= baseSpaces; countBaseSpaces++){
+                            System.out.print(" ");
+                        }
+                        for (int countBaseSymbols = 1; countBaseSymbols <= baseSymbols; countBaseSymbols++) {
+                            System.out.print("*");
+                        }
+                        System.out.println(" ");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter the number of lines tha you want to get on the tree: ");
+                    Scanner scanner2 = new Scanner(System.in);
+                    int maxLines = scanner2.nextInt();
+                    int spaces1 = maxLines - 1;
+                    int baseLines1 = maxLines/2;
+                    int baseSpaces1 = maxLines/2;
+                    int baseSymbols1  = maxLines -1;
+
+                    for (int countLines1 = 1, plusSymbols = 1; countLines1 <= maxLines; countLines1++, spaces1--, plusSymbols += 2) {
+                        for (int countSpaces = 0; countSpaces <= spaces1; countSpaces++) {
+                            System.out.print(" ");
+                        }
+                        for (int countSymbols = 1; countSymbols <= plusSymbols; countSymbols++) {
+                            System.out.print("*");
+                        }
+                        System.out.println(" ");
+                    }
+                    for (int countBaseLines = 1; countBaseLines <= baseLines1; countBaseLines++) {
+                        for (int countBaseSpaces = 0; countBaseSpaces <= baseSpaces1; countBaseSpaces++) {
+                            System.out.print(" ");
+                        }
+                        for (int countBaseSymbols = 1; countBaseSymbols <= maxLines; countBaseSymbols++) {
+                            System.out.print("*");
+                        }
+                        System.out.println(" ");
+                    }
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    exit = false;
             }
-            for(int countSymbols = (maxSymbols%2 == 1)? 1 : 2 ; countSymbols <= symbols; countSymbols++) {
-                System.out.print("*");
-            }
-            System.out.println(" ");
-
-            // es impar? countSymbols = 1 : countSymbols = 2
-
-            // Cada vez que aumenten los numeros se genera un espacio en los numeros impares, el espacio no se genera en los numeros pares
-            // si el residuo de la division del numero es igual a 1 el número es impar.  condicion = limite
-            // como del 5 llego 2
-            // es diferente para los numeros pares e impares.
-            // 1/2 = 0
-            // 2/2 = 1
-            // 3/2 = 1
-            // 4/2 = 2
-            // 5/2 = 2 + 1 = 3
-            //  --*
-            //  -***
-            //  *****
-            //
-        }
-
+        return exit;
     }
-
-    public void paintSymbolsByMaxLines() {
-        System.out.println("Enter the number of lines tha you want to get on the tree: ");
-        Scanner scanner = new Scanner(System.in);
-        int maxLines = scanner.nextInt();
-        int spaces = maxLines - 1 ;
-
-        for(int countLines = 1, plusSymbols = 1; countLines <= maxLines; countLines++, spaces--, plusSymbols+= 2) {
-            for(int countSpaces = 0; countSpaces <= spaces; countSpaces++) {
-                System.out.print(" ");
-            }
-            for(int countSymbols = 1; countSymbols <= plusSymbols; countSymbols++ ) {
-                System.out.print("*");
-            }
-            System.out.println(" ");
-        }
-    }
-
 }
